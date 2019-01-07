f6a4d4377e10644efd3dd5eb8000775ab27575f2
denniswouters <denniswouters@outlook.com>
2019-01-07 15:18:13 +0100

    Stuiterende bal toegevoegd, positionering juist en controls uitgelegd

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-  $(document).ready(function(){
-  $(window).scroll(function(e){
-  	parallaxScroll();
-	});
-	 
-	function parallaxScroll(){
-		var scrolled = $(window).scrollTop();
-		$('#achtergrond-1').css('top',(0-(scrolled*.25))+'px');
-		$('#achtergrond-2').css('top',(0-(scrolled*.4))+'px');
-        $('#achtergrond-3').css('top',(0-(scrolled*.55))+'px');
-	}
- 
- });
-
- 
+$(document).scroll(function () {
+var x = $(this).scrollLeft();
+if ((x > 1800) && (x < 2400)) {
+	$('img#tekstwolkje').fadeIn();
+  }  else {
+	$('img#tekstwolkje').fadeOut();
+  } 
+});
