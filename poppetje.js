219b6b0aa3ca50210731d9eb51b79207750408f9
denniswouters <denniswouters@outlook.com>
2019-01-15 11:34:41 +0100

    Comments toegevoegd zoals de docent vroeg

--- poppetje.js
+++ poppetje.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded",
+// Hieronder heb een functie gemaakt die .sprite stil laat staan wanneer de pijltjes naar links en rechts niet worden ingedrukt.
 function() {
-
 	var sprite = document.querySelector('.sprite'),
 		key = {left: false, right: false},
 		trans = 0,
 		property = getTransformProperty(sprite);
-
+// Dit zorgt ervoor dat de transities goed werken op zo goed als alle browsers (https://stackoverflow.com/a/9607205)
 	function getTransformProperty(element) {
 	    var properties = [
 	        'transform',
@@ -22,10 +22,12 @@ function() {
 	    }
 	    return false;
 	}
-
+// Een functie waarbij de sprite automatisch door repositionering op 0 px van de x-as staat.
 	function translate() {
 		sprite.style[property] = 'translateX(' + trans + 'px)';
 	}
+// Hieronder heb ik een functie die het personage laat lopen. Ik zeg eerst dat wanneer er een keycode in wordt gedrukt (pijltje
+// naar links is 37, naar rechts is 39) hij een toets eraan moet vinden en vervolgens zeg ik per toets wat de sprite moet doen. 
 	function walk(e) {
 		var keyCode = e.keyCode;
 		if (keyCode === 39) {
@@ -48,7 +50,7 @@ function() {
 			sprite.classList.add('walk-left');
 		}
 	}
-
+// Hier geef ik aan dat hij moet stoppen met lopen wanneer de toetsen worden losgelaten.
 	function stop(e) {
 		var keyCode = e.keyCode;
 		if (keyCode === 39) {
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Hieronder zeg ik dat wanneer het personage tussen de 1800 en 2400 pixels zit, een wolk moet laten zien.
 $(document).scroll(function () {
 var x = $(this).scrollLeft();
 if ((x > 1800) && (x < 2400)) {
