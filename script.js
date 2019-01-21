43ca5b907423b8a16a630efb5ac93ad952c16471
denniswouters <denniswouters@outlook.com>
2019-01-21 12:50:03 +0100

    Laatste push voor de zekerheid (assessment)

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Hieronder zeg ik dat wanneer het personage tussen de 1800 en 2400 pixels zit, een wolk moet laten zien.
 $(document).scroll(function () {
 var x = $(this).scrollLeft();
-if ((x > 1800) && (x < 2400)) {
-	$('img#tekstwolkje').fadeIn();
+if ((x > 3400) && (x < 4000)) {
+	$('img#tekstwolkje, #tekstinwolkje').fadeIn();
   }  else {
-	$('img#tekstwolkje').fadeOut();
+	$('img#tekstwolkje, #tekstinwolkje').fadeOut();
   } 
 });
